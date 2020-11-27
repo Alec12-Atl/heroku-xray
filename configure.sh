9372e8d5e57e7a627e68ed56b0ee500ce2ca7bd7
yuchen <74325297+yuchen198@users.noreply.github.com>
2020-11-28 00:20:01 +0800

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# Download and install xray
+mkdir /tmp/xray
+curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/download/latest/Xray-linux-64.zip
+unzip /tmp/xray/xray.zip -d /tmp/xray
+install -m 755 /tmp/xray/xray /usr/local/bin/xray
+
+# Remove temporary directory
+rm -rf /tmp/xray
+
+# xray new configuration
+install -d /usr/local/etc/xray
+cat << EOF > /usr/local/etc/xray/config.json
+{
+  "log": {
+    "loglevel": "none"
+  },
+  "inbounds": [
+    {
+      "port": 443,
+      "protocol": "VLESS",
+      "settings": {
+        "clients": [
+          {
+            "id": "$UUID",
+            "alterId": 0
+          }
+        ],
+        "decryption": "none"
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "/app"
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
+EOF
+
+# Run xray
+/usr/local/bin/xray -config /usr/local/etc/xray/config.json
